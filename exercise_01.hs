ce979910d70cc644b8d9dd1aca5ffd033836539f
Amy Boyer <amy.r.boyer@gmail.com>
2016-01-25 22:30:31 -0600

    solved first exercise

---
+++ exercise_01.hs
@@ -0,0 +1,15 @@
+import           Data.List
+
+multiples :: Integer -> [Integer] -> [Integer]
+multiples n = filter $  \x -> x `mod` n == 0
+
+mult3 :: [Integer]
+mult3 = multiples 3 [1..999]
+mult5 :: [Integer]
+mult5 = multiples 5 [1..999]
+
+mults :: [Integer]
+mults = mult3 `union` mult5
+
+main :: IO ()
+main = print $ sum mults
