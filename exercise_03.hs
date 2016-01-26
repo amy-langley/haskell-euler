d37620e6439a996c2040fddaf32921a869c9f575
Amy Boyer <amy@zooniverse.org>
2016-01-26 12:01:52 -0600

    solution to problem 3 that actually runs in a reasonable amount of time

--- exercise_03.hs
+++ exercise_03.hs
@@ -7,9 +7,20 @@ primes = sieve [2..]
 
 big :: Integer
 big = 600851475143
+-- big = 13195
 
-candidates :: [Integer]
-candidates = takeWhile  ( < big ) primes
+candidates :: Integer -> [Integer]
+candidates upto = takeWhile  ( < upto ) primes
+
+removePrimes :: Integer -> Integer -> [Integer] -> Integer
+removePrimes largest _ [] = largest
+removePrimes largest factorme (p:ps) =
+  if largest > factorme then largest
+  else if factorme `mod` p == 0 then (removePrimes p (factorme `div` p) (p:ps))
+  else removePrimes largest factorme ps
+
+factor :: Integer -> Integer
+factor x = removePrimes 1 x $ candidates x
 
 main :: IO ()
-main = print $ last $ [ x | x <- candidates, big `mod` x == 0 ]
+main = print $ factor big
